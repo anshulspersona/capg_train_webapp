ea658404a146cbc4ccaaca5a8a68abe8e7438fbf
aakashsr <anshul.anshul.sri@gmail.com>
2024-09-13 15:52:08 +0530

    adding config for aws deploy-new 32

--- src/test/java/com/mkyong/HelloControllerTests2.java
+++ src/test/java/com/mkyong/HelloControllerTests2.java
@@ -17,7 +17,7 @@ public class HelloControllerTests2 {
 
     @Test
     public void hello_ok() throws Exception {
-        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        ResponseEntity<String> response = template.getForEntity("/home", String.class);
         assertThat(response.getBody()).isEqualTo("Hello World, Spring Boot!");
     }
 
