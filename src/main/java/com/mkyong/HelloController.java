62f82422cbbee564d37ba9441b2e466a50356fbd
aakashsr <anshul.anshul.sri@gmail.com>
2024-09-13 16:09:36 +0530

    adding config for aws deploy-new 34

--- src/main/java/com/mkyong/HelloController.java
+++ src/main/java/com/mkyong/HelloController.java
@@ -1,12 +1,13 @@
 package com.mkyong;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     String hello() {
         return "Hello World, Spring Boot!";
     }
